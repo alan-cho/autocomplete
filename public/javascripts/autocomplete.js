5f729464428966c6f1c4e15d147b71eaf7c36aad
Alan Cho <alancho397@gmail.com>
2023-10-06 17:50:37 -0500

    throttle xhr for efficiency

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,3 +1,5 @@
+import debounce from "./debounce";
+
 const Autocomplete = {
   wrapInput: function () {
     let wrapper = document.createElement("div");
@@ -21,7 +23,7 @@ const Autocomplete = {
   },
 
   bindEvents: function () {
-    this.input.addEventListener("input", this.valueChanged.bind(this));
+    this.input.addEventListener("input", this.valueChanged);
     this.input.addEventListener("keydown", this.handleKeydown.bind(this));
     this.listUI.addEventListener("mousedown", this.handleMousedown.bind(this));
   },
@@ -161,8 +163,10 @@ const Autocomplete = {
 
     this.wrapInput();
     this.createUI();
-    this.bindEvents();
 
+    this.valueChanged = debounce(this.valueChanged.bind(this), 300);
+
+    this.bindEvents();
     this.reset();
   },
 };
